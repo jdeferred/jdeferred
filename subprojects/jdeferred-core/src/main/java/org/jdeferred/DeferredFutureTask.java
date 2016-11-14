d60c366a712feaccdb9dea16651bd64d010a1c47
aalmiray <aalmiray@gmail.com>
2016-11-14 13:14:21 +0100

    Update all license headers

--- subprojects/jdeferred-core/src/main/java/org/jdeferred/DeferredFutureTask.java
+++ subprojects/jdeferred-core/src/main/java/org/jdeferred/DeferredFutureTask.java
@@ -1,5 +1,5 @@
 /*
- * Copyright Ray Tsang ${author}
+ * Copyright 2013-2016 Ray Tsang
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
